8b6dadfcb422239aab3e10869292b4a7581bfea9
Antonio Ochoa <antonhub00@gmail.com>
2021-07-13 22:11:31 -0500

    Missing cors headers fixed

--- server/server.js
+++ server/server.js
@@ -4,6 +4,14 @@ const apiRouter = require('./routes')
 const app = express()
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*'])
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.append('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+})
+
 app.use('/cmv/api/', apiRouter)
 
 const PORT = process.env.CMV_DB_PORT || 3000
